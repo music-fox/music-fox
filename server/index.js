9dbcd27a73aa3417f09fd9240e15840b6fa9af0e
unknown <izra.arzi@gmail.com>
2020-09-03 15:13:01 +0700

    init

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const route = require('./routes')
+const {errHandler} = require('./middlewares/errhandler')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use('/', route)
+
+app.use(errHandler)
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ server/middlewares/errhandler.js
@@ -0,0 +1,5 @@
+const errHandler = (err) => {
+
+}
+
+module.exports = {errHandler}
