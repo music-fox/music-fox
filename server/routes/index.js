78ff5db649ea84ff24b0feef11ba059d9d1ddfd1
FaudzanAdzima <67786998+FaudzanAdzima@users.noreply.github.com>
2020-09-04 08:58:40 +0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,9 @@
 const route = require('express').Router()
 const UserController = require('../controllers/UserController')
 const IndexController = require('../controllers/indexController')
-<<<<<<< HEAD
-=======
+
 const SpotifyController = require('../controllers/musicController')
 const {getSpotifyAccessToken} = require('../middlewares/spotify')
->>>>>>> spotify
 
 route.post('/register', UserController.register)
 route.post('/login', UserController.login)
@@ -13,12 +11,9 @@ route.get('/user', UserController.list)
 route.get('/user/:id', UserController.showById)
 
 route.get('/', IndexController.home)
-<<<<<<< HEAD
-=======
 
 route.use(getSpotifyAccessToken)
 
 route.get('/test', SpotifyController.search)
->>>>>>> spotify
 
 module.exports = route
