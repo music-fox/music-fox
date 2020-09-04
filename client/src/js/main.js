e9254ec240bbb347b8d9dd2587aa412d247b8cc4
bryandaniswara <bryan.daniswara@gmail.com>
2020-09-04 15:39:40 +0700

    Fix client

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -171,6 +171,36 @@ function googleLogout() {
     });
 }
 
+function fetchPost() {
+    $.ajax({
+        url: 'http://localhost:3000/music-list',
+        method: 'get',
+        headers: {
+            token: localStorage.token
+        }
+    })
+
+        .done(user => {
+            user.Music.forEach(element => {
+                $('#post-container').append(`
+            <div class="column">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="content">
+                            <iframe src="https://open.spotify.com/embed/track/${element.trackId}" width="250" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <a href="#" class="card-footer-item">lyrics</a>
+                        <a href="#" class="card-footer-item">delete</a>
+                    </footer>
+                </div>
+            </div>`)
+            });
+        })
+    //console.log(data)
+}
+
 $(document).ready(function () {
     hideAll();
 
@@ -179,4 +209,6 @@ $(document).ready(function () {
     authHandler();
 
     googleLogin();
+
+    fetchPost();
 });
