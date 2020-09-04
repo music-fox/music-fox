9b218b4e6ec3d261e42b1219f9e77fac21994736
unknown <izra.arzi@gmail.com>
2020-09-04 10:10:57 +0700

    spotify 2nd commit

--- server/controllers/musicController.js
+++ server/controllers/musicController.js
@@ -1,15 +1,49 @@
 const {Music} = require('../models')
-// const { getSpotifyAccessToken } = require('..//spotify')
+const spotifySearch = require('../helpers/spotify')
 
 class SpotifyController {
-    static async search (req, res, next) {
+    static async add (req, res, next) {
         try {
-            let {query, type} = req.body
-            
+            let {track, artist} = req.body
+            let q = `${encodeURIComponent(`${track}` `${artist}`)}`
 
-            
+            let id = await spotifySearch(req.spotifyToken, q, 'track,artist')
+
+            if (id.error) {
+                throw id.error
+            }
+
+            let music = await Music.create({
+                musicUrl: id
+            })
+
+            res.status(200).json(music)
         } catch (err) {
-            res.send(err)
+            next(err)
+        }
+    }
+
+    static async delete (req, res, next) {
+        try {
+            let music = await Music.findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            if (!music) {
+                throw {message: 'Music not found', statusCode: 404}
+            }
+
+            await Music.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            return res.status(200).json(music)
+        } catch (err) {
+            next(err)
         }
     }
 }
--- server/routes/musicRoute.js
+++
@@ -1,8 +0,0 @@
-const route = require('express').Router()
-const {SpotifyController} = require('../controllers/musicController')
-
-route.get('/test', SpotifyController.test)
-
-module.exports = {
-    route
-}
