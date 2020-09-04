b8bf45d1ab3410a53611149eb8a41ebd53650e10
unknown <izra.arzi@gmail.com>
2020-09-04 08:15:50 +0700

    gatau knp harus user controllernya jg padahal ga disentuh

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,54 +1,22 @@
-const {User, Music} = require('../models')
-const { isValid } = require('../helpers/bcrypt')
-const { tokenGenerator } = require('../helpers/jwt')
+const { User } = require('../models')
+const mail = require('../helpers/mailgun')
 
 class UserController {
-    static list (req,res,next){
-        User.findAll()
-        .then(user=>{
-            res.status(200).json(user)
-        })
-        .catch(err=>{
-            next(err)
-        })
-    }
-    static login(req,res,next){
-        User.findOne({where:{email: req.body.email}})
-        .then(user=>{
-            if(user){
-                let valid = isValid(req.body.password, user.password)
-                if(valid){
-                    let access_token = tokenGenerator(user)
-                    let id = user.id
-                    let email = user.password
+    static async register(req, res, next) {
+        try {
+            const { username, email, password } = req.body
+            const newUser = await User.create({username, email, password})
+            mail(newUser.email)
+            return res.status(201).json({
+                id: newUser.id,
+                email: newUser.email
+            })
+        } catch (err) {
+            return next(err)
+        }
 
-                    res.status(200).json(id,email,access_token)
-                }
-            }else{
-                res.status(400).json({message: 'Invalid Username or Password'})
-            }
-        })
-        .catch(err=>{
-            res.status(400).json({message: 'Invalid Username or Password'})
-        })
     }
-    static showById (req,res,next){
-        User.findOne({
-            include: [Music],
-            where:{
-                id:req.params.id
-            }
-        })
-        .then(user=>{
-            res.status(200).json(user)
-        })
-        .catch(err=>{
-            res.status(500).json(err)
-        })
-    }
-    static register(req,res,next){
-        
-    }
+
 }
 
 module.exports = UserController
